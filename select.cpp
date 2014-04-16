c6d4a317d3fbbc4a46e3bbb5243e18a96b8bfbeb
James <jamesja@umich.edu>
2014-04-15 23:37:27 -0400

    merging

--- select.cpp
+++ select.cpp
@@ -17,6 +17,35 @@ Status Operators::Select(const string & result,      // name of the output relat
 		         const void *attrValue)     // literal value in the predicate
 {
 
-return OK;
-}
+	Status status = OK;
+	if(attr) //if attr is NULL, predicate is unconditional
+	{
+		AttrDesc attrDesc;
+		int reclen = 0;
 
+		attrCat->getInfo(inString, attr->attrName, attrDesc);
+
+		//Create projNames attrDescs
+		AttrDesc names[projCnt];
+		for(int i = 0; i < projCnt; i++)
+		{
+			AttrDesc attr;
+			attrCat->getInfo(result, projNames[i].attrName, attr);
+			names[i] = attr;
+		}
+
+		//If index exists on attribute in the predicate AND an equality, call IndexSelect
+		if(op == EQ && attrDesc.indexed)
+		{
+			//Need to find attrDesc
+			status = Operators::IndexSelect(result, projCnt, names, &attrDesc, op, attrValue, reclen);
+		}
+		else
+		{
+			//Need to find attrDesc and reclen
+			status = Operators::ScanSelect(result, projCnt, names, &attrDesc, op, attrValue, reclen);
+		}
+	}
+
+	return status;
+}
